225197b81f3f590717b180f6f6e48c9134597d4d
staxyo <noova13@gmail.com>
2023-02-21 19:40:54 +0100

    Removed carrypotter

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 let characters = ['Snylerdh', 'Sinjabusif', 'Jøyboy', 'Sizko', 'Qhrynne', 'Pititpeluche', 'Staxlock', 'Shikoroh', 
                     'Dustykai', 'Misakura', 'Nonolildrake', 'Bearjeww', 'Kujaalcoolic', 'Herarogue', 'Louisée', 'Daarkhër',
-                     'Surlegrill', 'Cùrtis', 'Carrypotter', 'Supxoxo'];
+                     'Surlegrill', 'Cùrtis', 'Supxoxo'];
 
 let altCharacters = ['Qhryme', 'Misasuke', 'Staxette', 'Stâx', 'Dustiny', 'Dustylish', 'Gigapuddi', 'Siskoshamn',
                        'Curtís', 'Misalamèche', 'Sisköe', 'Peluchevoker', 'Sñyler', 'Misarrow']
